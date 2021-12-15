edfbe897c9409a83ea5c34ed8a773c877d6edb50
Sequita Bridges <sequita.bridges.luol@statefarm.com>
2021-12-15 16:20:35 -0700

    GREEN: added SSH security group

--- main.tf
+++ main.tf
@@ -36,7 +36,8 @@ resource "aws_internet_gateway" "SR-igw" {
     Name = "SR-igw"
   }
 }
-#Create Route Tableresource "aws_route_table" "SR-RouteTable" {
+#Create Route Table
+resource "aws_route_table" "SR-RouteTable" {
    vpc_id = aws_vpc.SR-VPC.id 
    route {
       cidr_block = "0.0.0.0/0"
@@ -75,5 +76,40 @@ resource "aws_security_group" "allow_80" {
 
   tags = {
     Name = "allow_80"
+  }
+}
+
+# Create a Security Group for SSH
+resource "aws_security_group" "allow_22" {
+  name = "allow_22"
+  description = "allows SSH: 22"
+  vpc_id = aws_vpc.SR-VPC.id
+
+  ingress {
+    description = "SSH: 22 from VPC"
+    from_port = 22
+    to_port = 22
+    protocol = "TCP"
+    cidr_blocks = [aws_vpc.SR-VPC.cidr_block]
+  }
+
+   egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  tags = {
+      Name = "allow_22"
+  }
+}
+
+#Create EC2 instance
+resource "aws_instance" "SR-EC2-Instance" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
   }
 }
