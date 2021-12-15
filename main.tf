5f7c4c56bbb770ce43415f2c68f6a3bef10a13c5
Sequita Bridges <sequita.bridges.luol@statefarm.com>
2021-12-15 13:33:33 -0700

    GREEN: Created VPC and subnet

---
+++ main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Create a VPC
+resource "aws_vpc" "SR-VPC" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+      Name = "SR-VPC"
+  }
+}
+
+resource "aws_subnet" "SR-Subnet" {
+  vpc_id     = aws_vpc.SR-VPC.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "SR-Subnet"
+  }
+}
